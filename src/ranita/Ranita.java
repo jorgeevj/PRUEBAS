920f88be17e1cab8cc8182f4d0a5c57a087a25cd
ANFIBIO <ANFIBIO@RANSAP.mshome.net>
2015-09-03 05:19:42 -0500

    PRIMER COMIT

---
+++ src/ranita/Ranita.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ranita;
+
+/**
+ *
+ * @author ANFIBIO
+ */
+public class Ranita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   sapita ransap=new sapita();
+   ransap.setVisible(true);
+   //ransap.setVisible(false);
+  
+//    new sapita().setVisible(true);
+    }
+    
+}
